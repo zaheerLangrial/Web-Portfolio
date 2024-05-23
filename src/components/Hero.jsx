1f98e0bf5180fec422b4ac8dce4b46d685f061c9
Zaheer Ahmad <zaheerahmadlangrial92@gmail.com>
2024-05-24 00:51:55 +0500

    Create Navbar and continuue Hero section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { TypeAnimation } from 'react-type-animation'
+import ProfilePic from '../assets/Dp.png';
+
+const Hero = () => {
+  return (
+    <div>
+        <div>
+
+        </div>
+
+        <img src="" alt="" />
+    </div>
+  )
+}
+
+export default Hero
